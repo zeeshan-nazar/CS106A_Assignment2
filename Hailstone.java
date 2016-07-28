dbfffcfce4ba3d0ff66996d044a9d33380759fd4
zeeshan-nazar <mzeeshan.nazar@gmail.com>
2016-07-28 20:18:08 +0500

    problem 6 solution with decomposition

--- Hailstone.java
+++ Hailstone.java
@@ -9,39 +9,52 @@
 import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
-	public void run() {
-		/* You fill this in */
-		
-		//initialize all variables and input a no...
-		int count = 0;
-		int n = readInt("Enter a Number :");
+	
+	private void checkEvenOdd(int count, int number){
 		
 		while(true) //loop
 		{
 			//this handle when n is equal to 1 then terminate the program
-			if(n==1)
+			if(number==1)
 			{
 				println("The process took " +count+ " to reach !" );
 				break;
 			}
 			
 			//this condition to handle if no. is even...
-			else if(n%2==0)
+			else if(number%2==0)
 			{
-				print(n);
-				n=n/2;
-				println("is even so I take half: "+ n);
+				print(number);
+				number=number/2;
+				println(" is even so I take half: "+ number);
 			}
 			
-			//no. is odd ...
+			// this condition to handle if no. is odd ...
 			else
 			{
-				print(n);
-				n=(n*3)+1;
-				println("is odd, so I make 3n+1 :"+ n);
+				print(number);
+				number=(number*3)+1;
+				println(" is odd, so I make 3n+1: "+ number);
 			}
 			count++;
 		}
+	}
+	public void run() {
+		/* You fill this in */
+		
+		//initialize all variables and input a no...
+		int count = 0;
+		
+		while(true){
+		int number = readInt("Enter a Number :");
+		if(number == 0){
+			break;
+		}
+		else{
+			checkEvenOdd(count,number);
+		}
+	}
+		
 	}
 }
 
